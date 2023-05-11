bb81e27f1fc60ffa6dd88335a1fbd4514e9e4e3f
matty-zee <mzheng1108@gmail.com>
2023-05-11 00:21:08 -0700

    issues with modifuing htmlinner

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -10,7 +10,7 @@ class RecipeCard extends HTMLElement {
     // A1. TODO - Attach the shadow DOM to this Web Component (leave the mode open)
     let shadow = this.attachShadow({mode:'open'});
     // A2. TODO - Create an <article> element - This will hold our markup once our data is set
-    let div1 = document.createElement('div');
+    let article1 = document.createElement('article');
     // A3. TODO - Create a style element - This will hold all of the styles for the Web Component
     let style = document.createElement('style');
     // A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made
@@ -91,7 +91,7 @@ class RecipeCard extends HTMLElement {
     }`;
 
     // A5. TODO - Append the <style> and <article> elements to the Shadow DOM
-    shadow.appendChild(div1);
+    shadow.appendChild(article1);
     shadow.appendChild(style);
   }
 
@@ -122,34 +122,31 @@ class RecipeCard extends HTMLElement {
 
     // A6. TODO - Select the <article> we added to the Shadow DOM in the constructor
     let article = document.getElementsByTagName('article')[0];
+    console.log(article);
     // A7. TODO - Set the contents of the <article> with the <article> template given in
     //           cardTemplate.html and the data passed in (You should only have one <article>,
     //           do not nest an <article> inside another <article>). You should use Template
     //           literals (tempalte strings) and element.innerHTML for this.
-    article.innerHTML = `  <img src="https://link-to-article.com/recipe-thumbnail.jpg"
-    alt="Recipe Title">
-  <p class="title">
-    <a href="https://link-to-article.com">Title</a>
-  </p>
-  <p class="organization">The Chef's Organization</p>
-  <div class="rating">
-    <span>5</span>
-    <img src="/assets/images/icons/5-star.svg" alt="5 stars">
-    <span>(500)</span>
-  </div>
-  <time>50 min</time>
-  <p class="ingredients">
-    Comma, Separated, List, of, Ingredients
-  </p> `
+    article.innerHTML = `
+    <img src = "${data.imgSrc}" 
+      alt =  "${data.imgAlt}">
+    <p class = "title">
+      <a href = "${data.titleLnk}>${data.titleTxt}</a>
+    </p>
+    <p class="organization">${data.organization}</p>
+    <div class="rating">
+      <span>${data.rating}</span>
+      <img src="/assets/images/icons/5-star.svg" alt="5 stars">
+      <span>(${data.numRatings})</span>
+    </div>
+    <time>${data.lengthTime}</time>
+    <p class="ingredients">
+      ${data.ingredients}
+    </p>`
+    return;
   }
 }
 
 // A8. TODO - Define the Class as a customElement so that you can create
 //           'recipe-card' elements
-class customElement extends HTMLElement{
-  constructor(){
-    super();
-  }
-
-  
-}
+customElements.define('recipe-card', RecipeCard);
